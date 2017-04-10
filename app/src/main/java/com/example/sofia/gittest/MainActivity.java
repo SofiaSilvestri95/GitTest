8ffbe348122c6c468ca1a6986fe91eefed043325
sofia <sofia.silvestri@studenti.unina2.it>
2017-04-10 17:40:58 +0200

    Ho tradotto il saluto in italiano

--- app/src/main/java/com/example/sofia/gittest/MainActivity.java
+++ app/src/main/java/com/example/sofia/gittest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sofia.gittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Classe per l'activity principale
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
